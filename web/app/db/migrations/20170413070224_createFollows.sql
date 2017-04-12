530413d204416bb8d3671ea91d3073b7f66708d6
Kawamoto norifumi <mybest.is.next.one@gmail.com>
2017-04-13 07:15:57 +0900

    add insert

--- web/app/db/migrations/20170413070224_createFollows.sql
+++ web/app/db/migrations/20170413070224_createFollows.sql
@@ -2,7 +2,7 @@
 -- +goose Up
 -- SQL in section 'Up' is executed when this migration is applied
 CREATE TABLE Follow(id integer primary key, user_id integer, follow_id integer, created_at text, FOREIGN KEY(user_id) REFERENCES User(id), FOREIGN KEY(follow_id) REFERENCES User(id));
-
+INSERT INTO Follow VALUES(1, 1, 2, '2017-04-01 10:00:00');
 -- +goose Down
 -- SQL section 'Down' is executed when this migration is rolled back
 DROP TABLE Follow;
